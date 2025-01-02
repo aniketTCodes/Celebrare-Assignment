dbbfca2949b3e2c29ae91355a06479b6ebab22f6
Aniket Tiwari <ani962854.at@gmail.com>
2025-01-02 14:39:41 +0530

    added multiple screen and color picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,39 @@
 import 'package:celebrate_assignment/canvas.dart';
+import 'package:celebrate_assignment/canvas_change_notifier.dart';
+import 'package:celebrate_assignment/canvases_change_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+  final notifier1 = CanvasChangeNotifier();
+  final notifier2 = CanvasChangeNotifier();
+  final notifier3 = CanvasChangeNotifier();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text("Canvas"),
-        ),
-        body: Center(child: MyCanvas()),
-      ),
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: Text("Canvas"),
+          ),
+          body: ChangeNotifierProvider(
+            create: (context) => CanvasesChangeNotifier(),
+            child: Consumer<CanvasesChangeNotifier>(builder: (context, value, child) {
+              return PageView(
+                children: value.canvases.map<Widget>((e) => MyCanvas(),).toList(),
+                onPageChanged: (pageIdx) {
+                  value.changePage(pageIdx);
+                },
+              );
+            },),
+          )),
     );
   }
 }
